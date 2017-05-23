8b177d4543325288a279512e8b18b48444c0f48e
Wojciech Kaluza <wojciech.kaluza@hpe.com>
2017-05-23 16:00:02 +0100

    [FIND-841] Handle date ranges using ISO format
    [rev: Matthew Gordon]

--- src/to-field-text-node.js
+++ src/to-field-text-node.js
@@ -1,45 +1,57 @@
+/*
+ * Copyright 2015-2017 Hewlett Packard Enterprise Development Company, L.P.
+ * Licensed under the MIT License (the "License"); you may not use this file except in compliance with the License.
+ */
+
 define([
     'underscore',
     'fieldtext/js/field-text-parser',
-    './to-fields-and-values'
-], function (_, parser, toFieldsAndValues) {
-    "use strict";
+    './to-fields-and-values',
+    'moment'
+], function(_, parser, toFieldsAndValues, moment) {
+    'use strict';
 
     function escapeFieldTextValue(value) {
-        return _.reduce([',', '}', '{'], function (token) {
+        return _.reduce([',', '}', '{'], function(token) {
             return value.replace(token, encodeURIComponent(token))
         }, value);
+    }
+
+    function epochToIsoDate(epochArray) {
+        return _.map(epochArray, function(epoch) {
+            return moment(epoch * 1000).milliseconds(0).utc().format();
+        });
     }
 
     /**
      * Create a field text node from an array of parametric values. Returns null if the array is empty.
      * @return {parser.ExpressionNode}
      */
-    return function (parametricValuesArray) {
+    return function(parametricValuesArray) {
         var fieldsAndValues = toFieldsAndValues(parametricValuesArray);
 
         var fieldNodes = [];
-        _.each(fieldsAndValues, function (data, field) {
-            if (data.values && data.values.length > 0) {
-                var operator = data.type === 'Numeric' ? 'EQUAL' : 'MATCH';
+        _.each(fieldsAndValues, function(data, field) {
+            if(data.values && data.values.length > 0) {
+                var operator = data.type === 'Numeric'
+                    ? 'EQUAL'
+                    : 'MATCH';
                 fieldNodes.push(new parser.ExpressionNode(operator, [field], _.map(data.values, escapeFieldTextValue)));
             }
         });
-        
-        parametricValuesArray.forEach(function (data) {
-            if (data.range) {
-                if (data.type === 'Numeric') {
+
+        parametricValuesArray.forEach(function(data) {
+            if(data.range) {
+                if(data.type === 'Numeric') {
                     fieldNodes.push(new parser.ExpressionNode('NRANGE', [data.field], data.range));
                 } else {
-                    fieldNodes.push(new parser.ExpressionNode('RANGE', [data.field], [data.range[0] + 'e', data.range[1] + 'e']));
+                    fieldNodes.push(new parser.ExpressionNode('RANGE', [data.field], epochToIsoDate(data.range)));
                 }
             }
         });
 
-        if (fieldNodes.length) {
-            return _.reduce(fieldNodes, parser.AND);
-        } else {
-            return null;
-        }
+        return fieldNodes.length
+            ? _.reduce(fieldNodes, parser.AND)
+            : null;
     }
 });
--- test/spec/to-field-text-node.js
+++ test/spec/to-field-text-node.js
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015 Hewlett-Packard Development Company, L.P.
+ * Copyright 2015-2017 Hewlett Packard Enterprise Development Company, L.P.
  * Licensed under the MIT License (the "License"); you may not use this file except in compliance with the License.
  */
 
@@ -7,6 +7,7 @@ define([
     'backbone',
     'src/to-field-text-node'
 ], function(Backbone, toFieldTextNode) {
+    'use strict';
 
     describe('toFieldTextNode', function() {
         beforeEach(function() {
@@ -15,23 +16,38 @@ define([
                 {field: 'NAME', value: 'bob'},
                 {field: 'FRIEND', value: 'penny'},
                 {field: 'AGE', value: '25'},
-                {field: 'YEAR', range: [1307, 1327], type: 'Numeric'},
-                {field: 'DATE', range: [123456789, 123456790]}
+                {field: 'YEAR', range: [1307, 1327], type: 'Numeric'}
             ];
         });
 
         it('returns a field text node representing the collection', function() {
-            var node = toFieldTextNode(this.parametricValues);
-            var fieldText = node.toString();
+            this.parametricValues.push({field: 'DATE', range: [123456789, 123456790]});
+
+            var fieldText = toFieldTextNode(this.parametricValues).toString();
             expect(fieldText).toContain('MATCH{25}:AGE');
             expect(fieldText).toContain('MATCH{penny}:FRIEND');
             expect(fieldText).toContain('NRANGE{1307,1327}:YEAR');
-            expect(fieldText).toContain('RANGE{123456789e,123456790e}:DATE');
+            expect(fieldText).toContain('RANGE{1973-11-29T21:33:09Z,1973-11-29T21:33:10Z}:DATE');
+        });
+
+        it('treats 1 BC as year 0', function() {
+            this.parametricValues.push({
+                field: 'DATE',
+                range: [
+                    -1971 * 365 * 24 * 3600,
+                    -1941 * 365 * 24 * 3600
+                ]
+            });
+
+            var fieldText = toFieldTextNode(this.parametricValues).toString();
+            expect(fieldText).toContain('MATCH{25}:AGE');
+            expect(fieldText).toContain('MATCH{penny}:FRIEND');
+            expect(fieldText).toContain('NRANGE{1307,1327}:YEAR');
+            expect(fieldText).toContain('0000');
         });
 
         it('returns null for an empty collection', function() {
             expect(toFieldTextNode([])).toBeNull();
         });
     });
-
 });
